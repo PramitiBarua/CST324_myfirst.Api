f361ba2151bc8c844db06ed2dff5515e1b64bf92
pramitibarua <pramiti.barua@oit.edu>
2026-07-05 18:37:48 -0700

    SQL coonection factory created.Service added as singleton to API

--- CST324_myfirst.Api/Program.cs
+++ CST324_myfirst.Api/Program.cs
@@ -1,28 +1,53 @@
 
+using MyGuitarShop.Data.Ado.Factories;
+using System.Diagnostics;
+
 namespace CST324_myfirst.Api
 {
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
+        public static async void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            try
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                var builder = WebApplication.CreateBuilder(args);
+
+
+
+                AddServices(builder);
+
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                if (builder.Environment.IsDevelopment())
+                {
+                    builder.Services.AddEndpointsApiExplorer();
+                    builder.Services.AddSwaggerGen();
+                }
+
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+                ConfigureApplication(app);
+
+                await app.RunAsync();
             }
 
+            catch (Exception ex){
+                if (Debugger.IsAttached) Debugger.Break();
+                Console.WriteLine(ex.Message);
+            }
+            
+
+               
+        }
+
+        private static void ConfigureApplication(WebApplication app)
+        {
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
@@ -31,6 +56,17 @@ namespace CST324_myfirst.Api
             app.MapControllers();
 
             app.Run();
+        }
+
+        private static void AddServices(WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("MyGuitarShop");
+            //?? throw new InvalidOperationException("MyGuitarShop connectioon string not found. ");
+            // Add services to the container.
+
+            builder.Services.AddSingleton(new SqlConnectionFactory(connectionString));
+            builder.Services.AddControllers();
+
         }
     }
 }
